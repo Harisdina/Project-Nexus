973bb466f16d6d663391ba8b043464c87ca7108e
Harisdina <88874713+Harisdina@users.noreply.github.com>
2024-02-18 10:50:57 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,8 @@
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
